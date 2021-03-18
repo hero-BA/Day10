1471f770e362ae00771240e48fb434c9f3a10239
hero-BA <suad.hero@gmail.com>
2021-03-18 09:55:32 +0100

    Added exercise: Objects

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,13 +1,13 @@
 // Only change code below this line
 var myPet = {
-    species: "Dog",
-    name: "Roki",
-    legs: 4,
-    friends: ["Lesi", "Pujdo"]
+  species: "Dog",
+  name: "Roki",
+  legs: 4,
+  friends: ["Lesi", "Pujdo"],
 };
 function myFunction(myObj) {
-    return myObj;
-};
+  return myObj;
+}
 // Only change code above this line
 console.log(myFunction(myPet)); // Change this line
 module.exports = { myPet, myFunction };
