2e4766b888b3cf59dd83619274e7d074ed71f93a
hero-BA <suad.hero@gmail.com>
2021-03-18 10:25:27 +0100

    Modified exercise: .hasOwnProperty() method

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,15 +1,10 @@
 function myFunction(myObj, checkProp) {
-    // Only change code below this line
-    var myObj = {
-        title: "Titanic",
-        song: "My Heart Will Go On",
-        genre: "drama"
-    };
-    if(myObj.checkProp === true) {
-        return myObj.checkProp;
-    }
-    return "Not found";
-    // Only change code above this line
-};
-console.log(myFunction(myObj, "title"));
+  // Only change code below this line
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj[checkProp];
+  }
+  return "Not found";
+  // Only change code above this line
+}
+console.log(myFunction({ title: "Titanic", song: "My Heart Will Go On", genre: "drama" }, "song"));
 module.exports = myFunction;
