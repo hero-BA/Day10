5fe1ed70bdd8f38d478e67260acbb013dead56fd
hero-BA <suad.hero@gmail.com>
2021-03-18 10:03:39 +0100

    Added exercise: Complex objects

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,31 +1,30 @@
 function myFunction() {
-    var myMusic = {
-        001: {
-            artist: "Billy Joel",
-            title: "Piano Man",
-            release_year: 1973,
-            formats: {
-                1: "CD",
-                2: "8T",
-                3: "LP"
-            },
-            gold: true
-        },
-        002: {
-            // Add a record here
-            artist: "Dino Merlin", 
-            title: "Burek",
-            release_year: 2004,
-            formats: {
-                1: "CD",
-                2: "8T",
-                3: "LP"
-            },
-            gold: true
-        }
-    };
-    return myMusic;
-};
+  var myMusic = {
+    001: {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP",
+      },
+      gold: true,
+    },
+    002: {
+      // Add a record here
+      artist: "Dino Merlin",
+      title: "Burek",
+      release_year: 2004,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP",
+      },
+    },
+  };
+  return myMusic;
+}
 myFunction()[2];
 console.log(myFunction()[2]);
 module.exports = myFunction;
